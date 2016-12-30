84aff817cc5887b83d779754f8b0f04cd617befd
Sophia Gold <sophiagoldnyc@gmail.com>
2016-12-30 18:28:00 -0500

    updated dependencies and README accordingly

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject power-series "0.2.0"
+(defproject power-series "0.5.0"
   :description "library for computing power series using lazy evaluation"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/core.async "0.2.391"]
-                 [symbolic-algebra "0.1.0"]
+                 [symbolic-algebra "0.5.0"]
                  [criterium "0.4.4"]]
   :main ^:skip-aot power-series.core
   :target-path "target/%s"
